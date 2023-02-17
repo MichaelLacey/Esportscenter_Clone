f1042857dd92ce9b7f375773196b6f39d2792443
michaellacey <mikelacey123123@gmail.com>
2023-02-16 19:44:36 -0500

    testing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -14,19 +13,17 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
+    <BrowserRouter>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
+      
         <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
+          {/* <Route path="/login" > <LoginFormPage /> </Route>
+          <Route path="/signup"> <SignupFormPage /> </Route> */}
+
+
         </Switch>
-      )}
-    </>
+      
+    </BrowserRouter>
   );
 }
 
